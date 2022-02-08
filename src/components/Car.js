b7a4e1bde343b2fd3f7b1ba8a925c89695dc4e1f
Luis Mario <lmfg06@gmail.com>
2022-02-08 12:12:59 -0500

    Comentarios

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { BotonPagar, DivProducto, SpanNombre, SpanPrecio } from '../styles/Car';
 
 const Car = () => {
+
+    // Arrglo de objetos de los productos en el localStorage
     const productos = JSON.parse(localStorage.getItem('productCar')) || []
 
+    const navigate = useNavigate()
+
+    // Funcion cuando se da click en pagar este vacia el localStorage y lo redirecciona al inicio
     const pagar = () => {
         localStorage.removeItem('productCar')
-        window.location.href = "/"
+        navigate('/principal')
     }
 
     return <div>
@@ -18,14 +24,12 @@ const Car = () => {
         </header>
         <section>
             {
-                productos.map(prod => (
-                    <>
-                        <DivProducto>
-                            <img src={prod.imagen} alt='producto' />
-                            <SpanNombre>{prod.nombre}</SpanNombre>
-                            <SpanPrecio>$ {prod.precio} MXN</SpanPrecio>
-                        </DivProducto>
-                    </>
+                productos.map((prod, index) => (
+                    <DivProducto key={index}>
+                        <img src={prod.imagen} alt='producto' />
+                        <SpanNombre>{prod.nombre}</SpanNombre>
+                        <SpanPrecio>$ {prod.precio} MXN</SpanPrecio>
+                    </DivProducto>
                 ))
             }
             <DivProducto>
--- src/components/Seleccion.js
+++ src/components/Seleccion.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router';
 import { IoIosArrowBack } from 'react-icons/io'
 import { BsPlusCircle } from 'react-icons/bs'
 import { AiOutlineMinusCircle } from 'react-icons/ai'
@@ -7,38 +8,52 @@ import { urlBebidas, urlSabores } from '../helpers/Url';
 import { useParams } from 'react-router-dom';
 
 const Seleccion = ({ comida }) => {
-    const productos = JSON.parse(localStorage.getItem('productCar')) || []
-    const [cantidad, setcantidad] = useState(1);
-    const [precio1, setprecio1] = useState(25);
-    const [bebidas, setbebidas] = useState([]);
-    const [bebidaSelec, setbebidaSelec] = useState([]);
-    const [sabor, setsabor] = useState([]);
-    const { id } = useParams()
 
+    // Arrglo de objetos de los productos en el LocalStorage
+    const productos = JSON.parse(localStorage.getItem('productCar')) || []
+    // Estado de cantidad por defecto es 1
+    const [cantidad, setcantidad] = useState(1);
+    // Estado del precio del producto por defecto vale 25
+    const [precio1, setprecio1] = useState(25);
+    // Estado donde se guardan la informacion de las bebidas traidas desde la api
+    const [bebidas, setbebidas] = useState([]);
+    // Estado donde se guarda la informacion de la bebida seleccionada
+    const [bebidaSelec, setbebidaSelec] = useState([]);
+    // Estado donde se guarda la informacion del sabor traido de la api
+    const [sabor, setsabor] = useState([]);
+    // Id del producto que es traido del endpoint
+    const { id } = useParams()
+    // Navegacion
+    const navigate = useNavigate();
+
+    // Peticion para traer las bebidas y se guardan en el estado de bebidas
     const consultBebi = async () => {
         const resp = await fetch(urlBebidas)
         const data = await resp.json()
         setbebidas(data)
     }
 
+    // Peticion para traer los sabores que se guardan en el estado de sabores
     const consultSabo = async () => {
         const resp = await fetch(urlSabores)
         const data = await resp.json()
         setsabor(data)
     }
 
-
+    // Llama las funciones cuando detecta un cambio de estado
     useEffect(() => {
         consultBebi()
         consultSabo()
     }, []);
 
 
+    // Modifica el estado de la cantidad y el precio al presionar el boton mas
     const mas = () => {
         setcantidad(cantidad + 1)
         setprecio1(precio1 + 25)
     }
 
+    // Modifica el estado de la cantidad y el precio al presionar el boton menos
     const menos = () => {
         if (cantidad > 1) {
             setcantidad(cantidad - 1)
@@ -46,6 +61,7 @@ const Seleccion = ({ comida }) => {
         }
     }
 
+    // Agrega los productos al carrito y los manda al localStorage
     const agregarcar = () => {
         const productoComprar = {
             id,
@@ -55,16 +71,21 @@ const Seleccion = ({ comida }) => {
         }
 
         productos.push(productoComprar)
-        productos.push(bebidaSelec)
+        if (bebidaSelec.length !== 0) {
+            productos.push(bebidaSelec)
+        }
         localStorage.setItem('productCar', JSON.stringify(productos))
+        navigate('/car')
     }
 
+    // Agrega el producto de bebida al carrito de compras
     const bebidaSelect = (id, nombre, precio, imagen) => {
         setbebidaSelec({
             id, nombre, precio, imagen
         })
     }
 
+    // Hace el filtrado para mostrar el producto que se trae por paramentro desde el AppRouter
     const producto = comida.find((product) => product.id === Number(id))
     const { imgprincipal, nombrep } = producto
     console.log(nombrep)
